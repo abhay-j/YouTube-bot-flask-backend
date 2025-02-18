4d5bf938b128514aacb9709d1a0bd2a6a4b586a2
Abhay Joshi <jabhay2012@gmail.com>
2025-02-18 14:32:29 -0500

    port change reverted from 8080 to 5000

--- src/app.py
+++ src/app.py
@@ -106,5 +106,5 @@ def search():
 
 if __name__ == "__main__":
     # Use environment variables for host and port, default to 5000
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
